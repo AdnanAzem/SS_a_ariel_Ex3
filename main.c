ae59060b1db023192d8448f99877fcdda638de97
AdnanAzem <88532380+AdnanAzem@users.noreply.github.com>
2021-12-16 00:31:37 +0200

    Update main.c

--- main.c
+++ main.c
@@ -8,9 +8,10 @@
 char word[WORD];
 char txt[TXT];
 
+
+// Q1
 void writeWord(){
     char c = 0;
-    printf("Write Word:");
     scanf("%c", &c);
     int i = 0;
     for (; c != ' ' && c != '\t' && c != '\n' && i < WORD; i++){
@@ -22,7 +23,6 @@ void writeWord(){
 
 void writeTxt(){
     char c = 0;
-    printf("Write: ");
     scanf("%c", &c);
     int i;
     for (i=0; c != '~' && i < TXT; i++){
@@ -53,8 +53,7 @@ int CalcWord(char *c){
         int s = CharToNum(c[i]);
         ans = ans+s;
     }
-    return ans;
-    
+    return ans; 
 }
 
 int GematriaWord(){
@@ -109,3 +108,195 @@ void GematriaSentence(){
     }
     printf("\n");
 }
+
+
+//Q2
+char *AtbashWord(){
+    int i = 0;
+    char *ans = malloc(sizeof(char) * WORD);
+    if (ans == NULL)
+    {
+        printf("Memory Error");
+        exit(0);
+    }
+    while (word[i] != '\0' || i <= WORD)
+    {
+        if(word[i] >= 'a' && word[i] <= 'z'){
+            ans[i] = 'z' + 'a' - word[i];
+        }
+        else if(word[i] >= 'A' && word[i] <= 'Z'){
+            ans[i] = 'Z' + 'A' - word[i];
+        }
+        else{
+            ans[i] = word[i];
+        }
+        i++;
+    }
+    return ans;
+}
+
+char *ReverseAtbashWord(char *revWord){
+    char *ans = malloc(sizeof(char) * WORD);
+    int end = 0;
+    while (revWord[end] != '\0')
+    {
+        ++end;
+    }
+    for (int i = 0; i < end; i++)
+    {
+        ans[i] = revWord[end - i - 1];
+    }
+    ans[end] = '\0';
+    return ans;
+}
+int check(int index, char *Rword, char *txt) {
+	int i = 0,count=0;
+	while (txt[index] != 0) {
+		if (Rword[i]!='\0'&&Rword[i] == txt[index]) {
+			count++;
+			i++;
+			index++;
+		}
+		else {
+			break;
+			count = 0;
+		}
+	}
+	return count;
+}
+
+void AtbashSentence(){
+    printf("Atbash Sequences: ");
+    char *atbash = AtbashWord();
+    char *revAtbash = ReverseAtbashWord(atbash);
+    int i = 0;
+    int checkToPrint = 0;
+    while (txt[i] != '\0')
+    {
+        if(txt[i] == atbash[0]){
+            int count=check(i,atbash,txt);
+            if(count==strlen(word)){
+                    if(checkToPrint){
+                        printf("~");
+                    }
+                while(count!=0){
+                    
+                    printf("%c",txt[i]);
+                    i++;
+                    count--;
+                }
+                checkToPrint++;
+            }  
+        }
+        else if(txt[i] == revAtbash[0]){
+             int count=check(i,revAtbash,txt);
+            if(count==strlen(word)){
+                if(checkToPrint){
+                    printf("~");
+                }
+                while(count!=0){
+                    printf("%c",txt[i]);
+                    i++;
+                    count--;
+                }
+                checkToPrint++;
+            }
+
+        }
+    i++;
+    }
+    printf("\n"); 
+}
+
+
+
+// Q3
+void swap(char *a, char *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+
+void Sort(char arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+            }
+        }
+    }
+}
+
+void removeStr(char* str, char c){
+    int len = strlen(str);
+    for(int i = 0; i < len; i++){
+        if(str[i] == c){
+            for(int k = i; k < len; k++){
+                str[k] = str[k + 1];
+            }
+            len--;
+            i--;
+        }
+    }
+}
+
+void AnagramSentence(){
+    printf("Anagram Sequences: ");
+    int checkToPrint = 0;
+    char wSorted[strlen(word)];
+    strcpy(wSorted, word);
+    Sort(wSorted, strlen(wSorted));
+    for (int i=0; i<= strlen(txt); i++) 
+    {
+        if ((txt[i]!=' ') )
+        {
+            for (int j = i; j <= strlen(txt); j++) 
+            {
+                int len = j - i + 1;
+                char checkWord[len + 1];
+                int counter = 0;
+                for (int t = i; t < j; t++) 
+                {
+                    checkWord[counter] = txt[t];
+                    counter++;
+                }
+                checkWord[counter] = '\0';
+                char copyWord[strlen(checkWord)];
+                strcpy(copyWord, checkWord);
+                Sort(copyWord, strlen(copyWord));
+                removeStr(copyWord, ' ');
+                int comp = strcmp(copyWord, wSorted);
+                if (comp == 0) 
+                {
+                    if (checkToPrint) 
+                    {
+                        printf("~");
+                    }
+                    for (int k = 0; k < strlen(checkWord); k++) 
+                    {
+                        printf("%c", checkWord[k]);
+                    }
+                    checkToPrint++;
+                    break;
+                }
+            }
+        }
+    }
+    printf("\n");
+}
+
+
+int main()
+{
+    writeWord();
+    writeTxt();
+    GematriaSentence();
+    AtbashSentence();
+    AnagramSentence();
+    return 0;
+}
+
